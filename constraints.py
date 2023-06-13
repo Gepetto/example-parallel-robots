27392e78f5d3dc8aba489b8713a92bff850cd14f
Ludovic De Matteis <ldematteis@laas.fr>
2023-06-13 10:14:38 +0200

    Removed quaternion constraints

--- constraints.py
+++ constraints.py
@@ -38,49 +38,13 @@ def constraintResidual(model, data, cmodel, cdata, q=None, recompute=True, pinsp
     else:
         raise(NotImplementedError("Only 6D and 3D constraints are implemented for now"))
 
-# TODO clean quaternion constraints
-def constraintQuaternion(model, q, pinspace=pin):
-    """
-    L=constraintQuaternion(model, q)
-    Returns the list of the squared norm of each quaternion inside the configuration vector q (work for free flyer and spherical joint)
-
-    Arguments:
-        model - Pinocchio robot model
-        q - Joints configuration vector
-    Return:
-        L - List of quaternions norms
-    """
-    L = []
-    for j in model.joints:
-        idx_q = j.idx_q
-        nq = j.nq
-        nv = j.nv
-        if nq != nv:
-            quat = q[idx_q : idx_q + 4]
-            if pinspace is caspin:
-                L.append(casadi.norm_2(quat) ** 2 - 1)
-            else:
-                L.append(np.linalg.norm(quat)**2 - 1)
-    return L
-
-# TODO add constraintsPlanar
-
 def constraintsResidual(model, data, cmodels, cdatas, q=None, recompute=True, pinspace=pin, quaternions=False):
     res = []
     for cm, cd in zip(cmodels, cdatas):
         res.append(constraintResidual(model, data, cm, cd, q, recompute, pinspace))
 
-    if pinspace is pin:
-        if quaternions:
-            res.append(constraintQuaternion(model, q, pinspace))
-        return np.concatenate(res)
-    
-    elif pinspace is caspin:
-        if quaternions:
-            res += constraintQuaternion(model, q, pinspace)
-        return casadi.vertcat(*res)
-    else:
-        assert (False and "Should never happen")
+    return np.concatenate(res)
+
 
 import unittest
 class TestRobotLoader(unittest.TestCase):
