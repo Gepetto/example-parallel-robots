29b08fab541d615b5d5888399d3fde4c15c5c738
Ludovic De Matteis <ldematteis@laas.fr>
2023-07-28 10:46:40 +0200

    Major fixes in actuationModel

--- actuation_model.py
+++ actuation_model.py
@@ -13,7 +13,8 @@ class ActuationModel():
     
     """
     def __init__(self, model, names):
-        self.motname = names
+        self.motnames = names
+        self.model = model
         self.idMotJoints = []
         self.getMotId_q(model)
         self.getFreeId_q(model)
@@ -25,7 +26,7 @@ class ActuationModel():
         return(print("Id q motor: " + str(self.idqmot) + "\r" "Id v motor: " + str(self.idvmot) ))
     
 
-    def getMotId_q(self, model):
+    def getMotId_q(self):
         """
         GetMotId_q = (model)
         Return a list of ids corresponding to the configurations velocity associated with motors joints
@@ -36,16 +37,17 @@ class ActuationModel():
             Lid - List of motors configuration velocity ids
         """
         Lidq = []
-        for i, name in enumerate(model.names):
-            if name in self.motname:
-                self.idMotJoints.append(i)
-                idq=model.joints[i].idx_q
-                nq=model.joints[i].nq
-                for j in range(nq):
-                    Lidq.append(idq+j)
+        for i, name in enumerate(self.model.names):
+            for motname in self.motnames:
+                if motname in name:
+                    self.idMotJoints.append(i)
+                    idq = self.model.joints[i].idx_q
+                    nq = self.model.joints[i].nq
+                    for j in range(nq):
+                        Lidq.append(idq+j)
         self.idqmot=Lidq
 
-    def getMotId_v(self,model):
+    def getMotId_v(self):
         """
         GetMotId_q = (model)
         Return a list of ids corresponding to the configurations velocity associated with motors joints
@@ -56,12 +58,13 @@ class ActuationModel():
             Lid - List of motors configuration velocity ids
         """
         Lidv = []
-        for i, name in enumerate(model.names):
-            if name in self.motname:
-                idv=model.joints[i].idx_v
-                nv=model.joints[i].nv
-                for j in range(nv):
-                    Lidv.append(idv+j)
+        for i, name in enumerate(self.model.names):
+            for motname in self.motnames:
+                if motname in name:
+                    idv = self.model.joints[i].idx_v
+                    nv = self.model.joints[i].nv
+                    for j in range(nv):
+                        Lidv.append(idv+j)
         self.idvmot=Lidv
 
     def getFreeId_q(self,model):
