c7e00209a14ce73c667fac2318c9323f757deeae
Batto <vbatto@laas.fr>
2024-02-19 15:19:12 +0100

    add of actuation_data

--- example_parallel_robots/python/example_parallel_robots/actuation_model.py
+++ example_parallel_robots/python/example_parallel_robots/actuation_model.py
@@ -111,6 +111,63 @@ class ActuationModel():
                 Lidv.append(i)
         self.idvfree=Lidv
 
+
+import pinocchio as pin
+
+class ActuationData():
+    def __init__(self, model, constraint_model,actuation_model):
+
+        Lidmot=actuation_model.idvmot
+        Lidfree=actuation_model.idvfree
+        nv=model.nv
+        nv_mot=actuation_model.nv_mot
+        nv_free=actuation_model.nv_free
+        nc=0
+        for c in constraint_model:
+            nc+=c.size()
+
+        ## init different matrix
+        self.Smot=np.zeros((nv,nv_mot))
+        self.Sfree=np.zeros((nv,nv_free))
+        self.Jmot=np.zeros((nc,nv_mot))
+        self.Jfree=np.zeros((nc,nv_free))
+        self.Mmot=np.zeros((nv_mot,nv_mot))
+        self.dq=np.zeros((nv,nv_mot))
+
+        #PRIVATE
+        self.dq_no=np.zeros((nv,nv_mot))
+    
+        #init a list of constraint_jacobian
+        self.LJ=[np.array(())]*len(constraint_model)
+        constraint_data=[c.createData() for c in constraint_model]
+        data=model.createData()
+        for (cm,cd,i) in zip(constraint_model,constraint_data,range(len(self.LJ))):
+            self.LJ[i]=pin.getConstraintJacobian(model,data,cm,cd)
+
+
+        #selection matrix for actuated parallel robot
+        self.Smot[:,:]=0
+        self.Smot[Lidmot,range(nv_mot)]=1
+        self.Sfree[:,:]=0
+        self.Sfree[Lidfree,range(nv_free)]=1
+
+
+        # to delete
+        self.Jf_closed=pin.computeFrameJacobian(model,model.createData(),np.zeros(model.nq),0,pin.LOCAL)@self.dq
+
+
+        # init of different size of vector
+        self.vq=np.zeros(nv)
+        self.vqmot=np.zeros(nv_mot-6)
+        self.vqfree=np.zeros(nv_free)
+        self.vqmotfree=np.zeros(nv-6)
+
+        #list of constraint type
+        self.Lnc=[J.shape[0] for J in self.LJ]
+
+
+        #to delete ?
+        self.pinvJfree=np.linalg.pinv(self.Jfree)
 ########## TEST ZONE ##########################
 
 # Unitary tests for actuation model are included in the ones of loader_tools
